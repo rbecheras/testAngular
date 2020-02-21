3b79572111285d0a865708c31938691af5fddf7e
Rémi Becheras <remi@1play.co>
2020-02-21 19:36:37 +0100

    print payload if shouldAddToNewsletter

--- back/routes/contacts.js
+++ back/routes/contacts.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-router.post('/', function (req, res) {
-  res.json(req.body);
+router.post('/', function (req, res, next) {
+  if (req.body.shouldAddToNewsletter) {
+    console.log(req.body)
+  }
+  res.json({
+    status: 200,
+    message: 'success'
+  })
 });
 
 module.exports = router;
